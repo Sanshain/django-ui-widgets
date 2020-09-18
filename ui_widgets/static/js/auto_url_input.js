4a6a37f5ae9d81e40e452b992993e9a2f34b960e
Alexandr <digital-mag@ya.ru>
2020-09-18 18:55:33 +0300

    ui rename

--- ui_widgets/static/js/auto_url_input.js
+++
@@ -1,6 +0,0 @@
-function urlFieldPreFill(event){
-    if (event.inputType === 'deleteContentBackward') return;
-    if (!event.target.value.startsWith('http')){
-        event.target.value = 'https://' + event.target.value;
-    }
-}
